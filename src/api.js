f87397130dee9f26f51a44afbc5c09550c816822
dsalvin <alvin@sabanciuniv.edu>
2025-07-06 02:52:58 +0300

    log testing

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+console.log("API Base URL:", import.meta.env.VITE_API_BASE_URL);
+
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000',
 });
